6f8e3f2b44e81aaed8659c603b62125b64815b15
Eirik Nygaard <eirik.nygaard@yahooinc.com>
2023-08-31 09:43:53 +0200

    Add basic and multi-region examples

---
+++ examples/basic/main.tf
@@ -0,0 +1,26 @@
+#
+# Set up the GCP Terraform Provider to point to the GCP project where you want
+# to provision the Vespa Cloud Enclave.
+#
+provider "google" {
+  project = "<YOUR-GCP-PROJECT-HERE>"
+}
+
+#
+# Set up the basic module that grants Vespa Cloud permission to provision Vespa
+# Cloud resources inside the GCP project.
+#
+module "enclave" {
+  source      = "vespa-cloud/enclave/gcp"
+  version     = ">= 1.0.0, < 2.0.0"
+  tenant_name = "<YOUR-TENANT-HERE>"
+}
+
+#
+# Set up the VPC that will contain the Enclaved Vespa application.
+#
+module "zone_dev_us_central1_f" {
+  source  = "vespa-cloud/enclave/gcp"
+  version = ">= 1.0.0, < 2.0.0"
+  zone    = module.enclave.zones.dev.gcp_us_central1_f
+}
