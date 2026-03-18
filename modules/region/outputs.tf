126a2e1ff51a127e7ace6d0732510768ae729935
Håkon Hallingstad <hakon@vespa.ai>
2026-03-18 16:33:22 +0100

    Update addresses for overlapping resources between module versions

--- modules/region/outputs.tf
+++ modules/region/outputs.tf
@@ -14,12 +14,19 @@ output "zones" {
     for env, zone_map in var.region.zones :
     env => {
       for zone_key, zone_data in zone_map :
-      zone_key => merge(zone_data, {
+      zone_key => {
+        name             = "${zone_data.environment}.${zone_data.gcp_zone}"
+        environment      = zone_data.environment
+        region           = "gcp-${zone_data.gcp_zone}"
+        gcp_zone         = zone_data.gcp_zone
+        gcp_region       = zone_data.gcp_region
+        globals          = var.region.globals
+        template_version = var.region.template_version
         regional = {
           gcp_region      = var.region.gcp_region
           proxy_only_cidr = var.proxy_only_cidr
         }
-      })
+      }
     }
   }
 }
